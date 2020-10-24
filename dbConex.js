2036193b9accea2928767932d8e1d002b387c786
luber <lumacardonav@hotmail.com>
2020-10-24 11:45:47 -0500

    esctructura proyecto y conexion BD

---
+++ dbConex.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const seq = new Sequelize('bddelilahresto',process.env.USER, process.env.PASS,
+{
+    dialect:'mysql',
+    host: '127.0.0.1'
+});
+
+seq.authenticate().then(()=>{
+    console.log('Conectado a la BD');
+}).catch(err=>{
+    console.log(err);
+});
+
+module.exports = seq;
